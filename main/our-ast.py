23d62a16b86a10393d5bca5ce83557eb7a6be588
Andreas Heglingegård <andreas_heglingegaard@hotmail.com>
2019-09-11 11:05:11 +0200

    Progress

--- main/our-ast.py
+++ main/our-ast.py
@@ -1,5 +1,7 @@
 import ast
 import inspect
+import tests.fib
+import os
 
 from lib.fuzzingbook.ControlFlow import gen_cfg, to_graph
 from graphviz import Source
@@ -11,14 +13,57 @@ def fib(n,):
         l.append(l[-1]+l[-2])
     return l
 
-ast_node = ast.parse(inspect.getsource(fib))
+f = open("../tests/fib.py", "r")
+contents = f.read()
+
+#ast_node = ast.parse(inspect.getsource(fib))
+ast_node = ast.parse(contents)
 
 class RewriteName(ast.NodeTransformer):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.unique = 0
+
+    def unique_name(self, name):
+        return "name {}".format(self.unique_number)
+
+    def unique_number(self):
+        self.unique += 1
+        return self.unique
 
     def visit_FunctionDef(self, node):
         node.name = 'bob'
         self.generic_visit(node)
+
         return node
+
+    def visit_Assign(self, node):
+        #sub_nodes = self.generic_visit(node)
+        value_nodes = self.visit(node.value)
+        #print(sub_nodes)
+        #if isinstance(sub_nodes, ast.Assign):
+        #    print("assign")
+        if isinstance(value_nodes, [].__class__) and len(value_nodes) > 1:
+            print("assign value did return list")
+            new_node = ast.Assign(node.targets, value_nodes.pop(len(value_nodes - 1)))
+            return value_nodes.append(ast.copy_location(new_node, node))
+        else:
+            print("assign value did not return list")
+            new_node = ast.Assign(node.targets, value_nodes)
+            return ast.copy_location(new_node, node)
+
+    def visit_Attribute(self, node):
+        sub_nodes = self.visit(node.value)
+        if isinstance(sub_nodes, ast.Attribute):
+            prev_name = ast.copy_location(ast.Name(self.unique_name("u"), sub_nodes.ctx), sub_nodes)
+            prev_node = ast.Assign([prev_name], sub_nodes)
+            new_attr = ast.Attribute(prev_name, node.attr, node.ctx)
+            return [prev_node, new_attr]
+
+        return node
+
+
 
 ast_node = RewriteName().visit(ast_node)
 
