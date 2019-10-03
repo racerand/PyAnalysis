5cd75f4c5434cfcb78e932885cf0def2dce8d2ca
malthe <malthelange95@gmail.com>
2019-10-03 15:57:28 +0200

    Checkpoint

---
+++ tests/ConstructorClassAsHeap.py
@@ -0,0 +1,22 @@
+class Empt(object):
+    def e(self):
+        return
+
+class Fib(object):
+    def __init__(self, fst  , snd) -> None:
+        super().__init__()
+        self.fst = fst
+        self.snd = snd
+
+    def __call__(self, *args, **kwargs):
+        print("foo")
+    def foo(self, fooArg):
+        self.fst = Empt()
+        return fooVar
+def identity(z):
+        return z
+
+def makeEmpt(z):
+    return Empt()
+
+x = Fib(Emp(), Empt())
