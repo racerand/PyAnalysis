1ea5eb329272280cb40e29f13318d78d12845751
malthe <malthelange95@gmail.com>
2019-12-11 16:10:36 +0100

    small changes to chessboard

--- tests/evaluation/chessboard.py
+++ tests/evaluation/chessboard.py
@@ -7,6 +7,20 @@ import tempfile
 BLACK, WHITE = ("BLACK", "WHITE")
 
 
+def main():
+    checkers = CheckersBoard()
+    print(checkers)
+
+    chess = ChessBoard()
+    print(chess)
+
+    if sys.platform.startswith("win"):
+        filename = os.path.join(tempfile.gettempdir(), "gameboard.txt")
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(sys.stdout.getvalue())
+        print("wrote '{}'".format(filename), file=sys.__stdout__)
+
+
 if sys.platform.startswith("win"):
     def console(char, background):
         return char or " "
